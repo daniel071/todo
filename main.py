1b3f6593b41f1a654198f055eb13cc7e98a0b5c9
Daniel <daniel.pavela@gmail.com>
2022-05-21 19:07:28 +1000

    Implement basic UI

--- main.py
+++ main.py
@@ -4,11 +4,20 @@ gi.require_version('Gtk', '4.0')
 gi.require_version('Adw', '1')
 from gi.repository import Gtk, Adw
 
+@Gtk.Template.from_file("/home/daniel/code/todo/todo.ui")
+class headerbar(Gtk.HeaderBar):
+    __gtype_name__ = "headerbar"
+
+    aboutBtn = Gtk.Template.Child("aboutBtn")
+    print(aboutBtn)
+
+    @Gtk.Template.Callback("aboutBtn_button_clicked")
+    def aboutBtn_button_clicked(self, *args):
+        print("Hello!")
 
 class MainWindow(Gtk.ApplicationWindow):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
 
 class MyApp(Adw.Application):
     def __init__(self, **kwargs):
@@ -16,11 +25,19 @@ class MyApp(Adw.Application):
         self.connect('activate', self.on_activate)
 
     def on_activate(self, app):
-        self.win = MainWindow(application=app)
-        #self.win.present()
+        self.win = MainWindow(application=app) or headerbar(application=self)
+
         builder = Gtk.Builder()
-        builder.add_from_file("todo.ui")
-        window = builder.get_object("window1")
+        builder.set_current_object(self)
+        builder.add_from_file("/home/daniel/code/todo/todo.ui")
+        print(builder.get_objects())
+        #
+        # handlers = {
+        #     "onButtonPressed": onButtonPressed
+        # }
+        #builder.connect_signals(handlers)
+
+        window = builder.get_object("MainWindow")
         window.present()
 
 app = MyApp(application_id="com.example.GtkApplication")
