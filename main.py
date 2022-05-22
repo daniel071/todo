681321bd645eff13f173861d53fe09346d5455ba
Daniel <daniel.pavela@gmail.com>
2022-05-22 11:55:37 +1000

    Implement more UI

--- main.py
+++ main.py
@@ -12,6 +12,10 @@ class Handler:
         aboutDialog = builder.get_object("aboutDialog")
         aboutDialog.show_all()
 
+    def onAboutCloseBtnClicked(self, button):
+        aboutDialog = builder.get_object("aboutDialog")
+        aboutDialog.hide()
+
     def onSearchBtnToggled(self, button):
         toggleState = button.get_active()
         searchBar = builder.get_object("searchBar")
@@ -23,13 +27,29 @@ class Handler:
 
     def onCancelTaskBtnClicked(self, button):
         addDialog = builder.get_object("addDialog")
-        addDialog.close()
+        addDialog.hide()
 
     def onAddTaskBtnClicked(self, button):
         addDialog = builder.get_object("addDialog")
-        addDialog.close()
+        addDialog.hide()
 
+    def onOpenBtnToggled(self, button):
+        toggleState = button.get_active()
+        closedBtn = builder.get_object("closedBtn")
 
+        if toggleState == False:
+            closedBtn.set_active(True)
+        elif toggleState == True:
+            closedBtn.set_active(False)
+
+    def onClosedBtnToggled(self, button):
+        toggleState = button.get_active()
+        openBtn = builder.get_object("openBtn")
+
+        if toggleState == False:
+            openBtn.set_active(True)
+        elif toggleState == True:
+            openBtn.set_active(False)
 
 builder = Gtk.Builder()
 builder.add_from_file("todo.ui")
